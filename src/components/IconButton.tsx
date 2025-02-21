b8bf243082e8b0bdf8d15370d13c38aac8c01c1b
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2025-02-21 15:20:26 -0300

    Iniciando página de invites

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,11 +1,16 @@
-interface IconButtonProps {
-  children: React.ReactNode
-}
+import { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-export function IconButton(props: IconButtonProps) {
+interface IconButtonProps extends ComponentProps<'button'> {}
+
+export function IconButton({ className, ...props }: IconButtonProps) {
   return (
-    <button className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900">
-      {props.children}
-    </button>
+    <button
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className
+      )}
+      {...props}
+    ></button>
   )
 }
