f312215210a0b6c1ece8a1d8b4c0dd6ed08ae820
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2025-02-23 03:33:50 -0300

    Copiando link do invite

--- src/app/invite/invite-link-input.tsx
+++ src/app/invite/invite-link-input.tsx
@@ -1,18 +1,25 @@
+'use client'
+
 import { IconButton } from '@/components/IconButton'
 import { InputField, InputIcon, InputRoot } from '@/components/Input'
 import { Copy, Link } from 'lucide-react'
 
-export function InviteLikeInput() {
+interface InviteLikeInputProps {
+  inviteLink: string
+}
+
+export function InviteLikeInput({ inviteLink }: InviteLikeInputProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
   return (
     <InputRoot>
       <InputIcon>
         <Link className="size-5" />
       </InputIcon>
-      <InputField
-        readOnly
-        defaultValue="http://localhost:3000/invite/7677u85u34"
-      />
-      <IconButton className="-mr-2">
+      <InputField readOnly defaultValue={inviteLink} />
+      <IconButton className="-mr-2" onClick={copyInviteLink}>
         <Copy className="size-5" />
       </IconButton>
     </InputRoot>
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -5,6 +5,8 @@ import { Ranking } from './ranking'
 import { Stats } from './stats'
 
 export default function InvitePage() {
+  const inviteLink = 'http://localhost:3000/56465465u5646546'
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -30,7 +32,7 @@ export default function InvitePage() {
               inscrições:
             </p>
           </div>
-          <InviteLikeInput />
+          <InviteLikeInput inviteLink={inviteLink} />
           <Stats />
         </div>
       </div>
